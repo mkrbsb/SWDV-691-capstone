48443a69d533530da5b298db2ff47add96549da5
Eric <yellastar42@yahoo.com>
2021-07-25 22:53:24 -0500

    update

--- controller/login-controller.js
+++ controller/login-controller.js
@@ -4,48 +4,55 @@ const User = require("../model/users-model");
 require("dotenv").config();
 
 module.exports.login = (req, res) => {
-  const password = req.body.password;
-  const email = req.body.email;
+    const { password } = req.body;
+    const { email } = req.body;
 
-  User.findOne({ email }, async (err, user) => {
-    if (err) {
-      return;
-    }
-    if (user) {
-      if (await bcrypt.compare(password, user.password)) {
-        const token = jwt.sign({ user: user._id }, process.env.JWT_SECRET);
+    User.findOne({ email }, async (err, user) => {
+        if (err) {
+            return;
+        }
+        if (user) {
+            if (await bcrypt.compare(password, user.password)) {
+                const token = jwt.sign(
+                    { user: user._id },
+                    process.env.JWT_SECRET
+                );
 
-        return res.json({ token: token, isAuthenticated: true, user });
-      }
-      res.json({ err: { password: "password is incorrect" } });
-    }
-  });
+                return res.json({ token: token, isAuthenticated: true, user });
+            }
+            res.json({ err: { password: "password is incorrect" } });
+        }
+    });
 };
 
 module.exports.verify = async (req, res) => {
-  const token = req.body.token;
+    const { token } = req.body;
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findOne({ _id: decoded.user });
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            const user = await User.findOne({ _id: decoded.user });
 
-    res.json({ token: token, user, isAuthenticated: true });
-  } catch (err) {
-    // err
-    console.log(err);
-    res.json({ errors: { token: "Token is invalid" } });
-  }
+            res.json({ token: token, user, isAuthenticated: true });
+        } catch (err) {
+            // err
+            console.log(err);
+            res.json({ errors: { token: "Token is invalid" } });
+        }
+    } else {
+        res.json({ Token: "No Token" });
+    }
 };
 
 module.exports.create_user_login = (req, res, user) => {
-  const newUser = user;
-  const token = jwt.sign({ user: user._id }, process.env.JWT_SECRET);
+    const newUser = user;
+    const token = jwt.sign({ user: user._id }, process.env.JWT_SECRET);
 
-  newUser.password = "";
+    newUser.password = "";
 
-  return res.json({ token: token, user: newUser, isAuthenticated: true });
+    return res.json({ token: token, user: newUser, isAuthenticated: true });
 };
 
 module.exports.logout = (req, res) => {
-  res.json({ logout: "You have been logged out." });
+    res.json({ logout: "You have been logged out." });
 };
--- controller/users-controller.js
+++ controller/users-controller.js
@@ -8,93 +8,109 @@ const jwt = require("jsonwebtoken");
 const saltRounds = 10;
 
 module.exports.getAll = (req, res) => {
-  User.find({}, (err, data) => {
-    res.json(data);
-  });
+    User.find({}, (err, data) => {
+        res.json(data);
+    });
 };
 
 module.exports.create = async (req, res, next) => {
-  const email = req.body.user.email;
-  const password = req.body.user.password;
-  const vPassword = req.body.user.vPassword;
+    const email = req.body.user.email;
+    const password = req.body.user.password;
+    const vPassword = req.body.user.vPassword;
 
-  if (password !== vPassword) {
-    return;
-  }
-  const user = await User.findOne({ email });
-  if (user) {
-    res.status(400).json({ err: { email: "email account already used" } });
-    return;
-  }
+    if (password !== vPassword) {
+        return;
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+        res.status(400).json({ err: { email: "email account already used" } });
+        return;
+    }
 
-  const hashedPassword = bcrypt.hashSync(password, saltRounds);
+    const hashedPassword = bcrypt.hashSync(password, saltRounds);
 
-  const newUser = new User({
-    firstName: req.body.user.firstName,
-    lastName: req.body.user.lastName,
-    email,
-    password: hashedPassword,
-    phone: req.body.user.phone,
-    street_address1: req.body.user.street_address1,
-    street_address2: req.body.user.street_address2,
-    city: req.body.user.city,
-    state: req.body.user.state,
-    zipcode: req.body.user.zipcode,
-  });
+    const newUser = new User({
+        firstName: req.body.user.firstName,
+        lastName: req.body.user.lastName,
+        email,
+        password: hashedPassword,
+        phone: req.body.user.phone,
+        street_address1: req.body.user.street_address1,
+        street_address2: req.body.user.street_address2,
+        city: req.body.user.city,
+        state: req.body.user.state,
+        zipcode: req.body.user.zipcode,
+    });
 
-  const result = await newUser.save().catch((err) => console.log(err));
+    const result = await newUser.save().catch((err) => console.log(err));
 
-  const newWidget = new Widget({
-    id: result._id,
-  });
+    const newWidget = new Widget({
+        id: result._id,
+    });
 
-  await newWidget.save().catch((err) => console.log(err));
+    await newWidget.save().catch((err) => console.log(err));
 
-  // send back user and token
-  create_user_login(req, res, result);
+    // send back user and token
+    create_user_login(req, res, result);
 };
 
 module.exports.update = async (req, res) => {
-  const decoded = jwt.verify(req.body.token, process.env.JWT_SECRET);
-  const userId = decoded.user;
+    const decoded = jwt.verify(req.body.token, process.env.JWT_SECRET);
+    const userId = decoded.user;
 
-  if (userId) {
-    User.findOne({ _id: userId }, (err, user) => {
-      console.log(user);
-      if (user) {
-        res.status(400).json({ err: { email: "No user found" } });
-        return;
-      }
-      console.log(user);
-      // User.updateOne(
-      //   { _id: user._id },
-      //   { ...req.body.user },
-      //   { new: true },
-      //   (err, result) => {
-      //     if (err) {
-      //       console.log(err);
-      //     }
-      //     res.json({ message: "user updated" });
-      //   }
-      // );
-    });
-  }
+    if (userId) {
+        User.findOne({ _id: userId }, (err, user) => {
+            if (err) {
+                res.json(err);
+                return;
+            }
+            const updateUser = req.body.user;
+            if (!user) {
+                res.status(400).json({ errors: { email: "No user found" } });
+                return;
+            }
+            if (!updateUser.password) {
+                delete updateUser.password;
+                delete updateUser.vPassword;
+            } else {
+                if (updateUser.password !== updateUser) {
+                    res.status(400).json({
+                        errors: { password: "Passwords do not match" },
+                    });
+                }
+            }
+
+            delete updateUser.vPassword;
+            console.log(updateUser);
+            User.updateOne(
+                { _id: user._id },
+                { ...updateUser },
+                { new: true },
+                (err, result) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    res.json({ message: "user updated" });
+                }
+            );
+        });
+    }
 };
 
 module.exports.deleteOne = async (req, res) => {
-  const id = req.params.userId;
-  const deteledUser = await User.deleteOne({ _id: id }, (err) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    res.json({ user: "deleted user" });
-  });
+    const id = req.params.userId;
+    const deteledUser = await User.deleteOne({ _id: id }, (err) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        res.json({ user: "deleted user" });
+    });
 };
 
 module.exports.getOne = (req, res) => {
-  User.find({}, (err, data) => {
-    console.log(data);
-  });
-  res.json(req.params);
+    User.find({}, (err, data) => {
+        console.log(data);
+    });
+    res.json(req.params);
 };
